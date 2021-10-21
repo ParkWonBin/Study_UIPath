af36707747fd0fe31237ad6e852b20783cfc01cc
ParkWonBin <57087624+ParkWonBin@users.noreply.github.com>
2021-10-21 12:43:36 +0900

    Update KillExcel.vb

--- CustomFuncScript/KillExcel.vb
+++ CustomFuncScript/KillExcel.vb
@@ -1,8 +1,11 @@
 Dim Arr_process As process()
 Arr_process = Process.GetProcesses.Where(Function(x) x.ProcessName.ToUpper = "EXCEL").toArray
 If Arr_process.Count >0
+	console.writeline(">>> Kill Excel - 종료 후 2초 대기")
 	For Each p As process In Arr_process
 		p.kill()
 	Next
 	System.Threading.Thread.Sleep(2000)	'2초 딜레이
+Else 
+	console.writeline(">>> Kill Excel - 실행 중인 엑셀 없음.")
 End If
