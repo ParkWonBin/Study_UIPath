5f3bdd7626122e9117981d1b42de9807e4e36758
ParkWonBin <57087624+ParkWonBin@users.noreply.github.com>
2022-03-27 21:31:11 +0900

    파일 정리 -> Master Branch로 이동

--- RunvVB/test.vb
+++
@@ -1,8 +0,0 @@
-Module MainModule
-    Public Sub Main()
-        dim str_tmp as string
-        str_tmp = "123"
-        MsgBox("MsgBox(123) 입력 : " +str_tmp)
-        Console.WriteLine("Console.WriteLine(""123"") 입력 : "+str_tmp)
-    end Sub
-end Module 
