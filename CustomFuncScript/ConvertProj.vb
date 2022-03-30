da15685968935cbd38d262cc8e5ffe5764a07d5a
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2022-03-30 09:28:22 +0900

    Update ConvertProj.vb

--- CustomFuncScript/ConvertProj.vb
+++ CustomFuncScript/ConvertProj.vb
@@ -1,19 +1,29 @@
+if string.IsNullOrWhiteSpace(in_Str_ProjectCode)
+>>> input dialog
+>>> "Convertion IE -> Edge"
+>>> String.Format("in_Str_ProjectCode 가 입력되지 않았습니다.{0}IE->Edge 변환할 폴더를 선택해주세요.{0}{0}과제코드 입력시 :{0}폴더 설정{0}에서 파일을 추출합니다.{0}{0} 절대경로 입력시 해당 경로의 프로젝트를 변환합니다.",vbNewLine)
+>>> in_Str_ProjectCode
+End if
+Str_SourceDir = if(system.io.Directory.Exists(in_Str_ProjectCode),in_Str_ProjectCode, System.IO.Path.Combine("폴더 설정",in_Str_ProjectCode))
+
+if not System.IO.Directory.Exists( Str_SourceDir )
+    throw new BusinessRuleException( Str_SourceDir + " 폴더가 존재하지 않습니다." )
+End if
+    
+if string.IsNullOrWhiteSpace(in_Str_Result_FolderPath)
+        >>> "in_Str_Result_FolderPath 가 입력되지 않았습니다."+vbNewLine+" 변환 결과 파일을 확인할 폴더를 입력하세요."
+        >>> selectfolder :  in_Str_Result_FolderPath
+end if
+Str_ResultDir = System.IO.Path.Combine(in_Str_Result_FolderPath, if(system.io.Directory.Exists(in_Str_ProjectCode) ,system.io.Path.GetFileNameWithoutExtension(in_Str_ProjectCode), in_Str_ProjectCode) )
+'----------------------------------------
+'----------------------------------------        
+'----------------------------------------
+'----------------------------------------
+Dim Str_Dir_Source As String = in_Str_SourceDir
+Dim Str_Dir_Result As String = in_Str_ResultDir
+Dim int_Result_Cnt As Integer = 0
+
 '-----------------------------------------------
-' 폴더 입력받기 UI
-Dim Fnc_Get_DirPath As System.Func(Of String, String) = Function(str_Desc As String) As String
-    Dim folderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog = New System.Windows.Forms.FolderBrowserDialog() 
-
-    folderBrowserDialog1.Description = str_Desc
-    folderBrowserDialog1.ShowNewFolderButton = False
-    folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer
-
-    Dim result As System.Windows.Forms.DialogResult = folderBrowserDialog1.ShowDialog()
-    If result = System.Windows.Forms.DialogResult.OK Then
-            Return FolderBrowserDialog1.SelectedPath
-    End If 
-    Return ""
-End Function
-'-----------------------------------------------  
 ' 폴더 내 모든 파일 조회
 Dim Fnc_Get_All_Files As System.Func(Of String, String()) = Function(str_path As String) As String()
     Dim list_str_dir As New System.Collections.Generic.List(Of String) 
@@ -30,28 +40,24 @@ Dim Fnc_Get_All_Files As System.Func(Of String, String()) = Function(str_path As
 End Function
 '-----------------------------------------------
 ' main
-Dim StrArr_Before As String() = {"&lt;html html","&lt;html title","&lt;html idx","BrowserType=""IE"""}
-Dim StrArr_After As String() = {"&lt;html app='msedge.exe' html","&lt;html app='msedge.exe' title","&lt;html app='msedge.exe' idx","BrowserType=""Edge"""}
+Dim StrArr_Before As String() = {"&lt;html html","&lt;html title","&lt;html idx","&lt;html app='iexplore.exe'","BrowserType=""IE"""}
+Dim StrArr_After As String() = {"&lt;html app='msedge.exe' html","&lt;html app='msedge.exe' title","&lt;html app='msedge.exe' idx","&lt;html app='msedge.exe'","BrowserType=""Edge"""}
 Dim Str_ptn As String = "Selector=""\[[^]]+\]"""
 
-Dim Str_Description As String = "###############\n#IE -> Edge 변환 :#\n변환할 Project의 폴더를 선택해주세요.".replace("\n",VbNewLine)
-Dim Str_Desc_msg As String = "Attach Browser의 BrowserType을 IE에서 Edge로 바꿉니다."+VbNewLine
-Str_Desc_msg=Str_Desc_msg+"Selector 내 변수가 사용된 경우 {{ }}로 바꾸어줍니다."+VbNewLine+VbNewLine
-Str_Desc_msg=Str_Desc_msg+"before -> After"+VbNewLine+Join(StrArr_Before.Select(Function(x,i) x+" => "+StrArr_After(i) ).ToArray,VbNewLine)
-microsoft.visualbasic.interaction.msgbox(Str_Desc_msg)
-
-Dim Str_Dir_Source As String = Fnc_Get_DirPath(Str_Description)
-Dim Str_Dir_Result As String = Str_Dir_Source+"_Replaced"
-Dim int_Result_Cnt As Integer = 0
+Dim Str_Msg_title As String = "과제설명"
+Dim Str_Desc_msg As String = String.format("###########{0} 패키지 업데이트는 수동으로 진행하셔야 합니다.{0}파일명에 %20이 있는 경우 ' '으로 바꿔줍니다.{0}###########{0}{0}Attach Browser의 BrowserType을 IE에서 Edge로 바꿉니다.{0}",VbNewLine)
+Str_Desc_msg=Str_Desc_msg+"Selector 내 변수가 사용된 경우 {{ }}로 바꾸어줍니다."+VbNewLine+VbNewLine+"before -> After"+VbNewLine
+Str_Desc_msg=Str_Desc_msg+Join(StrArr_Before.Select(Function(x,i) x+" => "+StrArr_After(i) ).ToArray,VbNewLine)
+microsoft.visualbasic.interaction.msgbox(Str_Desc_msg,,Str_Msg_title)
 
 System.Console.WriteLine("변환 완료 : ")
 For Each Str_FilePath As String In Fnc_Get_All_Files(Str_Dir_Source)
     ' 파일 작성
-    Dim Str_File_ResultPath As String = Str_FilePath.replace(Str_Dir_Source,Str_Dir_Result)    
-    Str_File_ResultPath=Str_File_ResultPath.replace("%20"," ")
-    System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(Str_File_ResultPath))
-        
-    'xaml은 읽고 replace
+	Dim Str_File_ResultPath As String = Str_FilePath.replace(Str_Dir_Source,Str_Dir_Result)    
+	Str_File_ResultPath=Str_File_ResultPath.replace("%20"," ")
+	System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(Str_File_ResultPath))
+    
+    'xaml은 일괄적으로 변경
     If System.IO.Path.GetExtension(Str_FilePath).ToUpper = ".XAML"
         Dim Str_FileContent As String = System.IO.File.ReadAllText(Str_FilePath)
         
@@ -68,8 +74,8 @@ For Each Str_FilePath As String In Fnc_Get_All_Files(Str_Dir_Source)
 
         System.IO.File.WriteAllText(Str_File_ResultPath, Str_FileContent, System.Text.Encoding.UTF8)
     Else
-        System.IO.File.Copy(Str_Dir_Source, Str_File_ResultPath)
+        System.IO.File.Copy(Str_FilePath, Str_File_ResultPath)
     End If
     System.Console.WriteLine(int_Result_Cnt.Tostring("00")+" | "+Str_File_ResultPath)
 Next
-System.Diagnostics.Process.Start("explorer.exe", System.IO.Path.GetDirectoryName(Str_Dir_Result))
+System.Diagnostics.Process.Start("explorer.exe", Str_Dir_Result)
