7fa25461d4dd9d4db2593a05386e319f2c1d6536
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2022-04-01 07:16:15 +0900

    Update Fnc_Get_All_Files_From_Derectory.vb

--- Modules/Fnc_Get_All_Files_From_Derectory.vb
+++ Modules/Fnc_Get_All_Files_From_Derectory.vb
@@ -1,4 +1,5 @@
-Dim Fnc_Get_All_Files As System.Func(Of String, String()) = Function(str_path As String) As String
+Dim Fnc_Get_All_Files As System.Func(Of String, String()) = Function(str_path As String) As String()
+  ' 2022.03.30|wbpak|Get Files
   Dim list_str_dir As New System.Collections.Generic.List(Of String) 
   Dim list_str_file As New System.Collections.Generic.List(Of String)
 
@@ -9,5 +10,5 @@ Dim Fnc_Get_All_Files As System.Func(Of String, String()) = Function(str_path As
     list_str_dir.AddRange(System.IO.Directory.GetDirectories(str_path))
     list_str_file.AddRange(System.IO.Directory.GetFiles(str_path))
   End While
-  Return list_str_file
+  Return list_str_file.ToArray
 End Function
